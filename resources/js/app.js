2ae13b45b9b4e885dcf81016b78d12f27716c655
Lo0syash <arturdzhan2004@gmail.com>
2024-04-02 02:56:48 +0300

    save

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
+import { lte } from 'lodash';
 import './bootstrap';
 
 let ellipse = document.querySelectorAll('.ellipse');
 let slide = document.querySelectorAll('.banner-slide');
+
+for (let i = 0; i < ellipse.length; i++) {
+    ellipse[i].addEventListener('click', ()=> slider(i))
+}
+
 function slider(i) {
     ellipse.forEach(ellipses =>{
         ellipses.classList.remove('active')
@@ -13,7 +19,39 @@ function slider(i) {
     slide[i].classList.add('active')
 }
 
-for (let i = 0; i < ellipse.length; i++) {
-    ellipse[i].addEventListener('click', ()=> slider(i))
+let btnPrev = document.querySelector('.btn-prev');
+let btnNext = document.querySelector('.btn-next');
+let currentIndex = 0
+
+function showSlodes(index) {
+    slide.forEach((slides, i) => {
+        if (i === index) {
+            slides.classList.add('active')
+        } else {
+            slides.classList.remove('active')
+        }
+    })
+}
+
+showSlodes(currentIndex)
+
+document.querySelector('.btn-prev').addEventListener('click', ()=>{
+    currentIndex = (currentIndex - 1) % slide.length
+    showSlodes(currentIndex)
+    slider(currentIndex)
+})
+
+document.querySelector('.btn-next').addEventListener('click', ()=>{
+    currentIndex = (currentIndex + 1) % slide.length
+    showSlodes(currentIndex)
+    slider(currentIndex)
+})
+
+if (currentIndex === 0) {
+    btnPrev.classList.add('disabled')
+}
+
+if (currentIndex === slide.length - 1) {
+    btnNext.classList.add('disabled')
 }
 
